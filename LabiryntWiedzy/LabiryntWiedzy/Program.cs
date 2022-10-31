56f0f2b596c62c8ff58debe3579d66005228eb62
184677 <s184677@student.pg.edu.pl>
2022-10-31 20:50:44 +0100

    Utworzenie pustego projektu
    Pusty projekt C#

---
+++ LabiryntWiedzy/LabiryntWiedzy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabiryntWiedzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
